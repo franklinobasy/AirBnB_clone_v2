a9f119ba4a3f74f096c3724a4cf185726ccaacfb
Jesse <ibbraidejesse@gmail.com>
2022-10-10 19:36:09 +0100

    made pycodestyle compliant and declared do_create()

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -187,7 +187,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -319,6 +319,13 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
+    def do_create(self, arg):
+        """ allows for object creation with given parameters
+            cmd syntax: create <Class name> <param 1> <param 2> <param 3>...
+            param syntax: <key name>=<value>
+        """
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
