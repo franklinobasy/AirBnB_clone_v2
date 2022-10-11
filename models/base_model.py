3bff5cf9f8a158662059e13009e4bf390c7e2f44
Franklin Obasi <franklinobasy@gmail.com>
2022-10-11 22:31:16 +0100

    updated base_model state city

--- models/base_model.py
+++ models/base_model.py
@@ -76,4 +76,11 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary.keys():
+            del dictionary["_sa_instance_state"]
+            models.storage.save()
         return dictionary
+
+    def delete(self):
+        models.storage.delete(self)
+        models.storage.save()
