eae4236f3c47da71dd3f244337eed56b7a1088e4
Franklin Obasi <franklinobasy@gmail.com>
2022-10-12 20:17:11 +0100

    added classes to db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,6 +7,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 from models.base_model import Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class DBStorage:
