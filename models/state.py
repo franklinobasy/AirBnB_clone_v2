6beffa22abd9beece1ad9fbcd6e5839965ea3240
Franklin Obasi <franklinobasy@gmail.com>
2022-10-12 17:05:02 +0100

    task 6 - updated state.py

--- models/state.py
+++ models/state.py
@@ -4,6 +4,8 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+import models
+from models.city import City
 from models.base_model import BaseModel, Base
 
 
@@ -20,3 +22,15 @@ class State(BaseModel, Base):
 
     cities = relationship("City", backref="state",
                           cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        _cities = []
+
+        all_cities = models.storage.all(City)
+
+        for city_key in all_cities:
+            if all_cities[city_key].state_id == self.id:
+                _cities.append(all_cities[city_key].state_id)
+
+        return _cities
