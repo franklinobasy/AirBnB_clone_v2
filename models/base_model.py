e74004ded2edea48e67f2a08154557a7539fd0bd
Franklin Obasi <franklinobasy@gmail.com>
2022-10-12 19:32:48 +0100

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -49,12 +49,8 @@ class BaseModel:
                     setattr(self, key, value)
 
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
