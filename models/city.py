94066a76cf78a5e6de46c5a7788cdd4385b19498
Jesse <ibbraidejesse@gmail.com>
2022-10-13 04:30:57 +0100

    added relationship between places

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -19,3 +19,6 @@ class City(BaseModel, Base):
         ForeignKey("states.id"),
         nullable=False,
     )
+
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
