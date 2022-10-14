2f6d97a4ce674e767b6062fc007064b13c862412
Jesse <ibbraidejesse@gmail.com>
2022-10-14 20:11:55 +0100

    directly imported getenv, environ

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 
-import os
+from os import getenv, environ
 
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 
-if "HBNB_TYPE_STORAGE" in os.environ.keys() and \
-        os.getenv("HBNB_TYPE_STORAGE") == "db":
+if "HBNB_TYPE_STORAGE" in environ.keys() and \
+        getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
     storage = FileStorage()
