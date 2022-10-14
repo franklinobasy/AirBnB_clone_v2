398e6d915d128fa86828c6c818a4496db8c6f80b
Jesse <ibbraidejesse@gmail.com>
2022-10-14 17:05:22 +0100

    deleted amenities_id attr

--- models/place.py
+++ models/place.py
@@ -19,8 +19,3 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                               backref="place")
