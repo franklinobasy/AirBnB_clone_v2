73ea6762a27875c5106e7801aa22a8c5e3086c96
Jesse <ibbraidejesse@gmail.com>
2022-10-14 17:12:47 +0100

    troubleshooting double to single quotes

--- models/place.py
+++ models/place.py
@@ -9,10 +9,10 @@ from os import getenv
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60),  ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60),  ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
