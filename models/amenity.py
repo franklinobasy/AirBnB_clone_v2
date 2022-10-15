bf2305235e04848fa33d69de714325be6051512d
Jesse <ibbraidejesse@gmail.com>
2022-10-15 02:54:20 +0100

    refactored amenity

--- models/amenity.py
+++ models/amenity.py
@@ -4,11 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+s = "HBNB_TYPE_STORAGE"
+
 
 class Amenity(BaseModel, Base):
     """defines the amenity class"""
     __tablename__ = 'amenities'
-    if storage_type == 'db':
+    if s in environ.keys() and getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
     else:
         name = ""
