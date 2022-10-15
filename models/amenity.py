cc453da1efa2a7257c866fc54c4ed52be21f2c2f
Jesse <ibbraidejesse@gmail.com>
2022-10-15 02:57:49 +0100

    initialized amenity

--- models/amenity.py
+++ models/amenity.py
@@ -14,3 +14,7 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
