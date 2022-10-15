c73ae9b050f02628c67890d05be5d9e1dc0d4cbd
Jesse <ibbraidejesse@gmail.com>
2022-10-15 02:43:47 +0100

    added reviews and amenities relationship

--- models/place.py
+++ models/place.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import *
 from sqlalchemy.orm import relationship
 from os import getenv
+
+
+metadata = Base.metadata
+
+place_amenity = Table(
+    'place_amenity', metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'),
+           primary_key=True, nullable=False)
+)
 
 
 class Place(BaseModel, Base):
@@ -19,3 +29,8 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+
+    reviews = relationship('Review', backref='place',
+                           cascade='all, delete, delete-orphan')
+    amenities = relationship('Amenity', secondary=place_amenity,
+                             viewonly=False, backref='place_amenities')
