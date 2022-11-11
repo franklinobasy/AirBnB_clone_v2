5e2bdb00546a9facb22683f8d523e9e9f35f2fd7
Franklin Obasi <franklinobasy@gmail.com>
2022-11-11 14:59:49 +0100

    4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Function called with / route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Function called with /hbnb route """
+    return "HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ Function called with /c/<text> route """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """ Function called with /python/<text> route """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ Function called with /number/<n> route """
+    return "%d is a number" % n
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
