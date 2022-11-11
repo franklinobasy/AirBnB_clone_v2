9a0b21befa8772e4a965675a978a95193fd8812d
Franklin Obasi <franklinobasy@gmail.com>
2022-11-11 14:32:53 +0100

    3. Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Function called with / route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Function called with /hbnb route """
+    return "HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ Function called with /c/<text> route """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """ Function called with /python/<text> route """
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
