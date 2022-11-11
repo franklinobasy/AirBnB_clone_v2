dee8f6208d5418339c63b11962e3457dbf547de4
Franklin Obasi <franklinobasy@gmail.com>
2022-11-11 14:33:46 +0100

    2, C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,16 +7,19 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
+    """ Function called with / route """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """ Function called with /hbnb route """
     return "HBNB!"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
+    """ Function called with /c/<text> route """
     return f"C {text.replace('_', ' ')}"
 
 
