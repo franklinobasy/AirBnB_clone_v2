0c29bea6d9be46dd9f99611c2832e050072666c8
Franklin Obasi <franklinobasy@gmail.com>
2022-11-11 15:11:47 +0100

    5. Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Function called with / route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Function called with /hbnb route """
+    return "HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ Function called with /c/<text> route """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """ Function called with /python/<text> route """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ Function called with /number/<n> route """
+    return "%d is a number" % n
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
