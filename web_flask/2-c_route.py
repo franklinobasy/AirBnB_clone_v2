ad17645dbb0095b8e12be8319c2545a6c2769d51
Franklin Obasi <franklinobasy@gmail.com>
2022-11-11 14:17:28 +0100

    2. C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,13 +9,16 @@ app = Flask(__name__)
 def index():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB!"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     return f"C {text}"
+
 
 if __name__ == "__main__":
     app.run(port=5000)
