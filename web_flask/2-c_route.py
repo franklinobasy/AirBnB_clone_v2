d5b26f85b4603fa74d00d7670b78f723529b67e8
Franklin Obasi <franklinobasy@gmail.com>
2022-11-13 20:55:04 +0100

    2. C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" A script that starts a flask web application """
 from flask import Flask
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ def index():
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ Function called with /hbnb route """
-    return "HBNB!"
+    return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
@@ -24,4 +24,4 @@ def c(text):
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
