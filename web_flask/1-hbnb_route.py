d13e50d85e13f9ff867d2203b8c69008ad4f30ca
Franklin Obasi <franklinobasy@gmail.com>
2022-11-13 20:53:20 +0100

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,7 +14,7 @@ def index():
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ Function called with /hbnb route """
-    return "HBNB!"
+    return "HBNB"
 
 
 if __name__ == "__main__":
