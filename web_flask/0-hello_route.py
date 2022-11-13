d55b5be0d7e84ce832a61f5f99e975d24a1c26fb
Franklin Obasi <franklinobasy@gmail.com>
2022-11-13 20:50:24 +0100

    0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" A script that starts a flask web application """
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,4 +12,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
