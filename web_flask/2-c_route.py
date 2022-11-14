5a5399b8b84f0802c718fd84eeedc5c457b01493
Franklin Obasi <franklinobasy@gmail.com>
2022-11-14 10:44:48 +0100

    2. C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-""" A script that starts a flask web application """
+""" A script that starts a flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def index():
-    """ Function called with / route """
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Function called with /hbnb route """
-    return "HBNB"
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """ Function called with /c/<text> route """
-    return f"C {text.replace('_', ' ')}"
-
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
 
 if __name__ == "__main__":
     """ Main Function """
