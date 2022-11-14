9a704d432d65fea1aa6abe9edbc19939db2c5976
Franklin Obasi <franklinobasy@gmail.com>
2022-11-14 10:46:49 +0100

    2. C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,8 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ Prints a Message when /c is called """
-    return "C " + text.replace('_', ' ')
+    return f"C {text.replace('_', ' ')}"
+
 
 if __name__ == "__main__":
     """ Main Function """
